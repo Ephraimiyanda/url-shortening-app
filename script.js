5f03e435eb0eeeb2afb6c1c095ead4dd2e0c4cd8
Ephraim Iyanda <113598104+Ephraimiyanda@users.noreply.github.com>
2023-03-18 23:38:55 +0100

    done with challenge

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const OptionBtn= document.querySelector(".hamburger");
 const dropdownLinks = document.querySelector(".headingLinks");
 const myBtns = document.querySelector(".myBtns");
 const ShortenBtn = document.querySelector(".enterBtn");
-const input = document.querySelector("input");
+const input = document.querySelector(".input");
 const working =  document.querySelector(".working");
 const h1 =document.querySelector("h1");
 const middleSection =document.querySelector(".middleSection");
-
+const form  = document.querySelector("form")
 
 function showLinks(){
     dropdownLinks.classList.toggle("block");
@@ -16,35 +16,42 @@ function showLinks(){
 }
 OptionBtn.addEventListener("click",showLinks)
 
-
-function link(){
-     return input.textContent;
-    }
-const url = input.textContent
 //check for link
-async function urlShortening(url){
-    try{
-        const response = await fetch(`https//api.shrtcoi.de/v2/shorten?url=${url}`);
-        const data = await response.json();
-        const newLink  = document.createElement("div");
-        newLink.innerHTML= `
-        <div><p class="link">${data.result.ShortenBtn_link}</p><button class="copy"></button>
-        </div>
-        `
-        middleSection.appendChild(newLink);
-        if(!response.ok){
-            throw new error(`HTTP error:${response.status}`);
+
+
+ShortenBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const url = input.value;
+    shorten(url);
+    console.log(input.value);
+});
+
+async function shorten(url){
+
+        try{
+            const res= await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+            const data = await res.json();
+            const newLink  = document.createElement("div");
+            newLink.innerHTML= `<div class="linkDiv"><p class="originalLink">${data.result.original_link}</p><p class="shortenedLink">${data.result.short_link}</p><button class="copy">copy</button></div>`;
+            middleSection.prepend(newLink)
+            const copyBtn = document.querySelector(".copy");
+            input.value="";
+
+
+            copyBtn.addEventListener("click", ()=>{
+                copyBtn.classList.toggle("copied");
+                copyBtn.innerHTML=`copied!`
+                navigator.clipboard.writeText(data.result.short_link);
+            });
+
+
         }
-    }
-        catch(error){
-            console.error(`cannot find link data ${error}`)
-        }
+            catch(error){
+                console.error(`cannot find link data ${error}`)
+            }
+           
+        
+        
+
     }
 
-function shortenFunction(){
-    if(link() > 0){
-        urlShortening();
-    }
-}
-
-ShortenBtn.addEventListener("click", shortenFunction)
